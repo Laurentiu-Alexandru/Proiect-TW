310f3be98715cbbdde8397086f2a232e4acd99ee
bneiG1 <iosif.goga01@e-uvt.ro>
2022-11-12 00:40:24 +0200

    test 123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MainComponent } from "./main/main.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [{path: '', redirectTo: '/login', pathMatch: 'full'},
+{path: 'login', component: LoginComponent},
+{path: 'signup', component: SignupComponent},
+{path: 'main', component: MainComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports:[RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
