e6d98aef4df5bf780b758fa4fc6e2a9bb087c39a
MihaiFartala <mihai.fartala01@e-uvt.ro>
2023-01-10 17:06:20 +0200

    rating

--- src/app/application/main/main.component.ts
+++ src/app/application/main/main.component.ts
@@ -10,28 +10,66 @@ import { Restaurant } from '../../database-service/restaurant';
 })
 export class MainComponent {
 
-  restaurants: Restaurant[] =  [];
+  restaurants: Restaurant[] = [];
+  restaurants_comanda: Restaurant[] = [];
+  restaurants_rezervare: Restaurant[] = [];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private json: DatabaseService,
 
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.json.getRestaurants().subscribe((restaurants: Restaurant[])=>{
+    this.json.getRestaurants().subscribe((restaurants: Restaurant[]) => {
       this.restaurants = restaurants;
-      console.log(restaurants)
+      //console.log(restaurants)
+      for(let restaurant of restaurants)
+      {
+        this.checkType(restaurant);
+      }
+   
     })
   }
 
-  onRezerva(){
-    this.router.navigate(['../','restaurante'], {relativeTo: this.route});
+  onRezerva() {
+    this.router.navigate(['../', 'restaurante'], { relativeTo: this.route });
   }
 
   goToRestaurant(name: String) {
-    this.router.navigate(['../','restaurante', 'service'], { queryParams: { restaurant: name}, relativeTo: this.route});
- }
+    this.router.navigate(['../', 'restaurante', 'service'], { queryParams: { restaurant: name }, relativeTo: this.route });
+  }
+
+  checkType(restaurant: Restaurant) {
+    if (restaurant.type == "comanda") {
+      this.restaurants_comanda.push(restaurant);
+      this.sortResturants(this.restaurants_comanda);
+    }
+    else if (restaurant.type == "rezervare") {
+      this.restaurants_rezervare.push(restaurant);
+      this.sortResturants(this.restaurants_rezervare);
+    }
+  }
+
+  sortResturants(restaurante: Restaurant[]){
+   {
+       for(let i = 0; i < restaurante.length - 1; i++)
+       {
+           let min = i;
+           
+           for(let j = i + 1; j < restaurante.length; j++)
+           {
+               if(restaurante[j].rating > restaurante[min].rating)
+               {
+                   min = j;
+               }
+           }
+           const tmp = restaurante[min];
+           restaurante[min] = restaurante[i];
+           restaurante[i] = tmp;
+       }
+   }
+  }
 
 }
